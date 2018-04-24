47724e1869c8115d8ad3d3d0a7abfecee9d96263
Loïc Mahieu <mahieuloic@gmail.com>
2018-04-24 13:16:26 +0200

    init

---
+++ README.md
@@ -0,0 +1,99 @@
+# Automatic Snapshots for Google (gcloud) Compute Engine
+
+Bash script for Automatic Snapshots and Cleanup on Google Compute Engine based on label `auto-snapshot`.
+
+Highly inspirated by [jacksegal/google-compute-snapshot](https://github.com/jacksegal/google-compute-snapshot).
+
+## How it works
+gcloud-snapshot.sh will:
+
+- Get all the Disk IDs with label `auto-snapshot=true`
+- Take a snapshot of each Disk
+- The script will then delete all associated snapshots taken by the script for the Instance that are older than 7 days (optional: [default snapshot retention can be changed by using -d flag](#snapshot-retention))
+
+
+## Prerequisites
+* `cURL` must be installed
+* The VM must have the sufficient gcloud permissions, including "compute" set to "enabled":
+
+	[	http://stackoverflow.com/questions/31905966/gcloud-compute-list-networks-error-some-requests-did-not-succeed-insufficie#31928399](http://stackoverflow.com/questions/31905966/gcloud-compute-list-networks-error-some-requests-did-not-succeed-insufficie#31928399)
+
+
+## Installation
+
+ssh on to the server you wish to have backed up
+
+**Install Script**: Download the latest version of the snapshot script and make it executable:
+```
+cd ~
+wget https://raw.githubusercontent.com/jacksegal/google-compute-snapshot/master/gcloud-snapshot.sh
+chmod +x gcloud-snapshot.sh
+sudo mkdir -p /opt/google-compute-snapshot
+sudo mv gcloud-snapshot.sh /opt/google-compute-snapshot/
+```
+
+**To manually test the script:**
+```
+sudo /opt/google-compute-snapshot/gcloud-snapshot.sh
+```
+
+**Setup CRON**: You should then setup a cron job in order to schedule a daily backup. Example cron for Debian based Linux:
+```
+0 5 * * * root /opt/google-compute-snapshot/gcloud-snapshot.sh >> /var/log/cron/snapshot.log 2>&1
+```
+
+Please note: the above command sends the output to a log file: `/var/log/cron/snapshot.log` - instructions for creating & managing the log file are below.
+
+**Manage CRON Output**: You should then create a directory for all cron outputs and add it to logrotate:
+
+- Create new directory:
+```
+sudo mkdir /var/log/cron
+```
+- Create empty file for snapshot log:
+```
+sudo touch /var/log/cron/snapshot.log
+```
+- Change permissions on file:
+```
+sudo chgrp adm /var/log/cron/snapshot.log
+sudo chmod 664 /var/log/cron/snapshot.log
+```
+- Create new entry in logrotate so cron files don't get too big :
+```
+sudo nano /etc/logrotate.d/cron
+```
+- Add the following text to the above file:
+```
+/var/log/cron/*.log {
+    daily
+    missingok
+    rotate 14
+    compress
+    notifempty
+    create 664 root adm
+    sharedscripts
+}
+```
+
+## Snapshot Retention
+By default snapshots will be kept for 7 days, however they can be kept for longer / shorter, by using the the -d flag:
+
+    Usage: ./snapshot.sh [-d <days>]
+
+    Options:
+
+       -d  Number of days to keep snapshots. Snapshots older than this number deleted.
+           Default if not set: 7 [OPTIONAL]
+
+## License
+
+MIT License
+
+Copyright (c) 2018 Jack Segal
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ gcloud-snapshot.sh
@@ -0,0 +1,320 @@
+#!/usr/bin/env bash
+export PATH=$PATH:/usr/local/bin/:/usr/bin
+
+
+
+
+###############################
+##                           ##
+## INITIATE SCRIPT FUNCTIONS ##
+##                           ##
+##  FUNCTIONS ARE EXECUTED   ##
+##   AT BOTTOM OF SCRIPT     ##
+##                           ##
+###############################
+
+
+#
+# DOCUMENTS ARGUMENTS
+#
+
+usage() {
+  echo -e "\nUsage: $0 -p <project> [-d <days>]" 1>&2
+  echo -e "\nOptions:\n"
+  echo -e "    -p    Project name."
+  echo -e "          [REQUIRED]"
+  echo -e "    -d    Number of days to keep snapshots.  Snapshots older than this number deleted."
+  echo -e "          Default if not set: 7 [OPTIONAL]"
+  echo -e "\n"
+  exit 1
+}
+
+
+#
+# GETS SCRIPT OPTIONS AND SETS GLOBAL VAR $OLDER_THAN
+#
+
+setScriptOptions()
+{
+    while getopts ":d:p:" o; do
+      case "${o}" in
+        d)
+          opt_d=${OPTARG}
+          ;;
+
+        p)
+          opt_p=${OPTARG}
+          ;;
+
+        *)
+          usage
+          ;;
+      esac
+    done
+    shift $((OPTIND-1))
+
+    if [[ -n $opt_p ]];then
+      PROJECT_ID=$opt_p
+    else
+      >&2 echo "Invalid project name"
+      exit 1
+    fi
+
+    if [[ -n $opt_d ]];then
+      OLDER_THAN=$opt_d
+    else
+      OLDER_THAN=7
+    fi
+}
+
+
+#
+# RETURNS LIST OF DEVICES
+#
+# input: ${INSTANCE_NAME}
+#
+
+getDeviceList()
+{
+    echo "$(gcloud --project=$PROJECT_ID compute disks list --filter 'labels.auto-snapshot = true' --format='value(name)')"
+}
+
+
+#
+# RETURN ZONE OF DEVISE
+#
+# input: ${DEVISE_NAME}
+#
+
+getDeviceZone()
+{
+    echo "$(gcloud --project=$PROJECT_ID compute disks list --filter name="$1" --format='value(zone)')"
+}
+
+
+#
+# RETURNS SNAPSHOT NAME
+#
+
+createSnapshotName()
+{
+    # create snapshot name
+    local name="gcas-$1-$2"
+
+    # google compute snapshot name cannot be longer than 62 characters
+    local name_max_len=62
+
+    # check if snapshot name is longer than max length
+    if [ ${#name} -ge ${name_max_len} ]; then
+
+        # work out how many characters we require - prefix + device id + timestamp
+        local req_chars="gcas--$2"
+
+        # work out how many characters that leaves us for the device name
+        local device_name_len=`expr ${name_max_len} - ${#req_chars}`
+
+        # shorten the device name
+        local device_name=${1:0:device_name_len}
+
+        # create new (acceptable) snapshot name
+        name="gcas-${device_name}-$2" ;
+
+    fi
+
+    echo -e ${name}
+}
+
+
+#
+# CREATES SNAPSHOT AND RETURNS OUTPUT
+#
+# input: ${DEVICE_NAME}, ${SNAPSHOT_NAME}, ${INSTANCE_ZONE}
+#
+
+createSnapshot()
+{
+    echo -e "$(gcloud --project=$PROJECT_ID compute disks snapshot $1 --snapshot-names $2 --zone $3)"
+}
+
+
+#
+# GETS LIST OF SNAPSHOTS AND SETS GLOBAL ARRAY $SNAPSHOTS
+#
+# input: ${SNAPSHOT_REGEX}
+# example usage: getSnapshots "(gcas-.*${INSTANCE_ID}-.*)"
+#
+
+getSnapshots()
+{
+    # create empty array
+    SNAPSHOTS=()
+
+    # get list of snapshots from gcloud for this device
+    local gcloud_response="$(gcloud --project=$PROJECT_ID compute snapshots list --filter="name~'"$1"'" --uri)"
+
+    # loop through and get snapshot name from URI
+    while read line
+    do
+        # grab snapshot name from full URI
+        snapshot="${line##*/}"
+
+        # add snapshot to global array
+        SNAPSHOTS+=(${snapshot})
+
+    done <<< "$(echo -e "$gcloud_response")"
+}
+
+
+#
+# RETURNS SNAPSHOT CREATED DATE
+#
+# input: ${SNAPSHOT_NAME}
+#
+
+getSnapshotCreatedDate()
+{
+    local snapshot_datetime="$(gcloud --project=$PROJECT_ID compute snapshots describe $1 | grep "creationTimestamp" | cut -d " " -f 2 | tr -d \')"
+
+    #  format date
+    echo -e "$(date -d ${snapshot_datetime%?????} +%Y%m%d)"
+
+    # Previous Method of formatting date, which caused issues with older Centos
+    #echo -e "$(date -d ${snapshot_datetime} +%Y%m%d)"
+}
+
+
+#
+# RETURNS DELETION DATE FOR ALL SNAPSHOTS
+#
+# input: ${OLDER_THAN}
+#
+
+getSnapshotDeletionDate()
+{
+    echo -e "$(date -d "-$1 days" +"%Y%m%d")"
+}
+
+
+#
+# RETURNS ANSWER FOR WHETHER SNAPSHOT SHOULD BE DELETED
+#
+# input: ${DELETION_DATE}, ${SNAPSHOT_CREATED_DATE}
+#
+
+checkSnapshotDeletion()
+{
+    if [ $1 -ge $2 ]
+
+        then
+            echo -e "1"
+        else
+            echo -e "2"
+
+    fi
+}
+
+
+#
+# DELETES SNAPSHOT
+#
+# input: ${SNAPSHOT_NAME}
+#
+
+deleteSnapshot()
+{
+    echo -e "$(gcloud --project=$PROJECT_ID compute snapshots delete $1 -q)"
+}
+
+
+logTime()
+{
+    local datetime="$(date +"%Y-%m-%d %T")"
+    echo -e "$datetime: $1"
+}
+
+
+#######################
+##                   ##
+## WRAPPER FUNCTIONS ##
+##                   ##
+#######################
+
+
+createSnapshotWrapper()
+{
+    # log time
+    logTime "Start of createSnapshotWrapper"
+
+    # get date time
+    DATE_TIME="$(date "+%s")"
+
+    # get a list of all the devices
+    DEVICE_LIST=$(getDeviceList)
+
+    # create the snapshots
+    echo "${DEVICE_LIST}" | while read DEVICE_NAME
+    do
+        # create snapshot name
+        SNAPSHOT_NAME=$(createSnapshotName ${DEVICE_NAME} ${DATE_TIME})
+
+        # get devise zone
+        ZONE=$(getDeviceZone ${DEVICE_NAME})
+
+        # create the snapshot
+        OUTPUT_SNAPSHOT_CREATION=$(createSnapshot ${DEVICE_NAME} ${SNAPSHOT_NAME} ${ZONE})
+    done
+}
+
+deleteSnapshotsWrapper()
+{
+    # log time
+    logTime "Start of deleteSnapshotsWrapper"
+
+    # get the deletion date for snapshots
+    DELETION_DATE=$(getSnapshotDeletionDate "${OLDER_THAN}")
+
+    # get list of snapshots for regex - saved in global array
+    getSnapshots "gcas-.*"
+
+    # loop through snapshots
+    for snapshot in "${SNAPSHOTS[@]}"
+    do
+        # get created date for snapshot
+        SNAPSHOT_CREATED_DATE=$(getSnapshotCreatedDate ${snapshot})
+
+        # check if snapshot needs to be deleted
+        DELETION_CHECK=$(checkSnapshotDeletion ${DELETION_DATE} ${SNAPSHOT_CREATED_DATE})
+
+        # delete snapshot
+        if [ "${DELETION_CHECK}" -eq "1" ]; then
+           OUTPUT_SNAPSHOT_DELETION=$(deleteSnapshot ${snapshot})
+        else
+           echo "No need to delete ${snapshot}"
+        fi
+
+    done
+}
+
+
+
+
+##########################
+##                      ##
+## RUN SCRIPT FUNCTIONS ##
+##                      ##
+##########################
+
+# log time
+logTime "Start of Script"
+
+# set options from script input / default value
+setScriptOptions "$@"
+
+# create snapshot
+createSnapshotWrapper
+
+# delete snapshots older than 'x' days
+deleteSnapshotsWrapper
+
+# log time
+logTime "End of Script"
